08291af161ce726f1083a04454c0ba3934f1be4e
Ayush Krishnatray - a0k09v3 <Ayush.Krishnatray@walmart.com>
2024-09-03 12:18:11 +0530

    feat: adding routes and checks

---
+++ frontend/src/Screens/Landing.tsx
@@ -0,0 +1,15 @@
+const Landing = () => {
+  return (
+    <div className="grid grid-cols-6 flex flex-col align-middle py-8 px-8">
+      <div className="col-span-4 flex justify-center">
+        <img src="./chessBoard.png" alt="Chessboard Image" height={"500px"} width={"500px"}/>
+      </div>
+      <div className="col-span-2 bg-slate-900 flex flex-col justify-center">
+        <h1 className="py-4 text-slate-50 text-3xl">Play Chess Online</h1>
+        <button className="py-2 px-4 btn rounded bg-lime-100 max-w-48">Play</button>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
